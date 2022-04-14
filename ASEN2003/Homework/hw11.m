cd51201a6195a84467b0d8d94a0e3fe7424bacb0
dunningtonthomas <dunningtonthomas@gmail.com>
2022-04-13 22:00:28 -0600

    Update hw11.m

--- ASEN2003/Homework/hw11.m
+++ ASEN2003/Homework/hw11.m
@@ -3,16 +3,29 @@ close all; clear; clc;
 %% Plotting the step response of a 2nd order LTI System
 
 syms x(t);
-x(t) = 0.111 - 0.136*exp(-t)*sin(2.45*t + 1.23);
-xss = 0.111;
-nineXss = 0.111 * 0.9;
-oneXss = 0.111 * 0.1;
-ans1 = solve(0.111 - 0.136*exp(-t)*sin(2.45*t + 1.23) == nineXss, t);
+x(t) = 1 - 1.061*exp(-t)*sin(2.828*t + 1.23);
+xss = 1;
+nineXss = 1 * 0.9;
+oneXss = 1 * 0.1;
 
 time = linspace(0,10,1000);
+xFunc = x(time);
+[~,ind1] = min(abs(xFunc - oneXss));
+[~,ind2] = min(abs(xFunc - nineXss));
+t1 = time(ind1);
+t2 = time(ind2);
 
-plot(time, x(time), 'lineWidth', 2);
+plot(time, xFunc, 'lineWidth', 2);
+hold on
+xline(0.1602);
+xline(0.6170);
 xlabel('Time (s)');
 ylabel('Response (x(t))');
 title('Response Plot');
 grid on
+
+%% Step Response
+G = tf([9], [1 2 9]);
+figure(2)
+S = stepinfo(G, 'SettlingTimeThreshold', 0.05);
+step(G)
