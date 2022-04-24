853c8e042df2e3db315fb5ebb9bd648150176e00
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2022-04-24 17:27:12 -0600

    Update

--- ASEN2004/lab2P2/Altimeter/polyfix.m/polyfix.m
+++ ASEN2004/lab2P2/Altimeter/polyfix.m/polyfix.m
@@ -1,31 +1,3 @@
-clear; close all; clc;
-
-%% Reading in Data
-data = readmatrix('altData');
-time = data(:,1);
-altPress = data(:,2);
-alt = (1 - (altPress ./ 1013.25).^(1/5.2559)) ./ 0.0000225577; %Alt in meters
-
-%% Truncating 
-timeLog = time > 1.52 & time < 6.44;
-time = time(timeLog);
-time = time - time(1);
-alt = alt(timeLog);
-alt = alt - alt(1);
-
-coeff = polyfix(time, alt, 3, 0, 0);
-plotAlt = polyval(coeff, time);
-%% Plotting 
-figure(1)
-plot(time, alt, 'linewidth', 1.25)
-hold on
-grid on
-plot(time, plotAlt, 'linewidth', 2)
-title('Altimeter Data');
-xlabel('Time (s)');
-ylabel('Height (m)');
-legend('Raw Data', 'Polynomial Fit');
-%% Func
 function [p,S,mu] = polyfix(x,y,n,xfix,yfix,xder,dydx)
 %POLYFIX Fit polynomial p to data, but specify value at specific points
 %
