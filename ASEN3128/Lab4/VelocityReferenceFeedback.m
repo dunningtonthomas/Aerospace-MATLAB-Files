edd3d5080ba56dd50ad173ae0d1cd32662aced45
dunningtonthomas <dunningtonthomas@gmail.com>
2022-10-12 19:24:29 -0600

    update

---
+++ ASEN3128/Lab4/VelocityReferenceFeedback.m
@@ -0,0 +1,12 @@
+function [Fc,Gc] = VelocityReferenceFeedback(t,var)
+%VELOCITYREFERENCEFEEDBACK This function takes as input the time t and the
+%12 by 1 state matrix in var
+%It then calculates the forces and moments required to move the quadrotor
+%to the desired reference position by applying a reference velocity
+
+
+
+
+
+end
+
--- ASEN3128/Lab4/problem5.m
+++ ASEN3128/Lab4/problem5.m
@@ -19,6 +19,7 @@ I = [Ix, 0, 0; 0, Iy, 0; 0, 0, Iz];
 %Longitudinal
 k1Long = 0.0016;
 k2Long = 0.0029;
+k3Long = 0;
 
 %Lateral
 k1Lat = 0.0013;
@@ -27,13 +28,17 @@ k3Lat = 0;
 
 
 %Setting up the A matrix for lateral dynamics
-A = [0, g, 0; 0, 0, 1; -k3Lat, -k2Lat, -k1Lat];
+Alat = [0, g, 0; 0, 0, 1; -k3Lat, -k2Lat, -k1Lat];
+
+%Setting up the A matrix for longitudinal dynamics
+Along = [0, -g, 0,; 0, 0, 1; -k3Long, -k2Long, -k1Long];
 
 %Setting up various k3 values
 k3Values = linspace(-0.001,0.001,1000);
 
 %Eig returns 3 by 1 eigenvalues
-eigenVec = zeros(length(k3Values), 3);
+eigenVecLat = zeros(length(k3Values), 3);
+eigenVecLong = zeros(length(k3Values), 3);
 
 %Calculating the eigenvalues
 %Imaginary tolerance
@@ -42,30 +47,61 @@ tolerance = 0.025;
 
 for i = 1:length(k3Values)
     k3Lat = k3Values(i);
-    A = [0, g, 0; 0, 0, 1; -k3Lat/Ix, -k2Lat/Ix, -k1Lat/Ix];
-    eigenVec(i,:) = eig(A)';
+    k3Long = k3Values(i);
+    Alat = [0, g, 0; 0, 0, 1; -k3Lat/Ix, -k2Lat/Ix, -k1Lat/Ix];
+    Along = [0, -g, 0,; 0, 0, 1; -k3Long/Iy, -k2Long/Iy, -k1Long/Iy];
+    eigenVecLat(i,:) = eig(Alat)';
+    eigenVecLong(i,:) = eig(Along)';
     
     %Try to find the k3 value that gives us three eigenvalues that are
     %negative and all real within some tolerance
-    imagComponents = imag(eigenVec(i,:));
-    realComponents = real(eigenVec(i,:));   
+    imagComponents = imag(eigenVecLat(i,:));
+    realComponents = real(eigenVecLat(i,:));
+    
+    realComponentsLong = real(eigenVecLong(i,:));
+    
+    if(realComponentsLong(1) == -1.04722 || realComponentsLong(2) == -1.04722 || realComponentsLong(3) == -1.04722)
+        x = 5
+    end
 end
 
+
 %Getting the eigenvalue for k3
-finalK3Value = k3Values(554);
-A = [0, g, 0; 0, 0, 1; -finalK3Value/Ix, -k2Lat/Ix, -k1Lat/Ix];
-eigenFinal = eig(A);
-scatter(real(eigenFinal), imag(eigenFinal));
+%Lateral
+finalK3ValueLat = k3Values(554);
+Alat = [0, g, 0; 0, 0, 1; -finalK3ValueLat/Ix, -k2Lat/Ix, -k1Lat/Ix];
+eigenFinalLat = eig(Alat);
 
-%Plotting the root locus plot
+
+%Longitudinal
+finalK3ValueLong = k3Values(431);
+Along = [0, -g, 0; 0, 0, 1; -finalK3ValueLong/Iy, -k2Long/Iy, -k1Long/Iy];
+eigenFinalLong = eig(Along);
+scatter(real(eigenFinalLong), imag(eigenFinalLong));
+
+
+%Plotting the root locus plots
+%Lateral dynamics
 figure();
-scatter(real(eigenVec(:,1)), imag(eigenVec(:,1)), 'r');
+scatter(real(eigenVecLat(:,1)), imag(eigenVecLat(:,1)), 'r');
 hold on
-scatter(real(eigenVec(:,2)), imag(eigenVec(:,2)), 'r');
-scatter(real(eigenVec(:,3)), imag(eigenVec(:,3)), 'r');
-scatter(real(eigenFinal), imag(eigenFinal), 'g', 'filled');
+scatter(real(eigenVecLat(:,2)), imag(eigenVecLat(:,2)), 'r');
+scatter(real(eigenVecLat(:,3)), imag(eigenVecLat(:,3)), 'r');
+scatter(real(eigenFinalLat), imag(eigenFinalLat), 'g', 'filled');
+
+title('Lateral');
 
 
+
+%Longitudinal
+figure();
+scatter(real(eigenVecLong(:,1)), imag(eigenVecLong(:,1)), 'r');
+hold on
+scatter(real(eigenVecLong(:,2)), imag(eigenVecLong(:,2)), 'r');
+scatter(real(eigenVecLong(:,3)), imag(eigenVecLong(:,3)), 'r');
+scatter(real(eigenFinalLong), imag(eigenFinalLong), 'g', 'filled');
+
+title('Longitudinal');
 
 
 
