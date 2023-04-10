81c60f6fe43d27d7cef2ddfb538b4b6079d3f8ea
dunningtonthomas <dunningtonthomas@gmail.com>
2023-04-09 20:54:34 -0600

    Update

---
+++ ASEN3300/Labs/Lab10/preLab.m
@@ -0,0 +1,48 @@
+%% Clean up
+clear; close all; clc;
+
+%% Problem 5
+%Plot the amplitude eand phase response of sallen key filter for given set
+%of R and C values
+%Output f0, Q, and plot the filter magnitude repsonse, the filter phase
+%response, from DC to 60 kHz, use dB scale for magnitude and degrees, use
+%log scale for the frequency
+R = 335;
+C1 = 1e-6;
+C2 = 0.1e-6;
+
+Q = 0.5 * sqrt(C1 / C2);
+w0 = 1 / (R*sqrt(C1*C2));
+f0 = 1 / (2*pi) * w0;
+
+magResponse = @(w)(20*log10((w0^2 ./ (sqrt((w0^2 - w.^2).^2 + w0^2 * w.^2 ./ Q^2))))); %dB
+phaseResponse = @(w)((-1*atan(w*R*sqrt(C1*C2))) * 180/pi); %Degrees
+
+%Create frequency vector
+freqVec = linspace(0,60*1000);
+
+%Output result
+fprintf('f0 = %f\n', f0);
+fprintf('Q = %f\n', Q);
+
+%Plotting
+figure();
+set(0, 'defaulttextinterpreter', 'latex');
+subplot(2,1,1);
+semilogx(freqVec, magResponse(freqVec), 'linewidth', 2, 'color', 'b');
+grid on
+
+xlabel('Frequency (Hz)');
+ylabel('Magnitude Response (dB)');
+sgtitle('Sallen-Key Bode Plot');
+
+subplot(2,1,2);
+semilogx(freqVec, phaseResponse(freqVec), 'linewidth', 2, 'color', 'r');
+grid on
+
+xlabel('Frequency (Hz)');
+ylabel('Phase Response $$(^{\circ})$$');
+
+
+
+
