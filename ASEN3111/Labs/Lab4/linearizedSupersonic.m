de210469c4f79c7c7111897dc500af467151e69d
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2023-04-27 14:36:22 -0600

    Update linearizedSupersonic.m

--- ASEN3111/Labs/Lab4/linearizedSupersonic.m
+++ ASEN3111/Labs/Lab4/linearizedSupersonic.m
@@ -22,24 +22,11 @@ alpha = alpha * pi / 180;
 c_l = 4*alpha / sqrt(M^2 - 1);
 
 %Calculate cd
-%Define length 1 over c and length 2 over c using law of sines
-L1oC = sind(epsilon2) / sind(180 - epsilon1 - epsilon2);
-L2oC = sind(epsilon1) / sind(180 - epsilon1 - epsilon2);
-
-%Lower surface g value, derived the integral result by hand
-gl_Squared = L1oC*cosd(epsilon1)*((tand(epsilon1))^2 - (tand(epsilon2))^2) + L2oC*cosd(epsilon2)*(tand(epsilon2))^2;
-
-%Upper surface is the same as upper
-gu_Squared = L1oC*cosd(epsilon1)*((tand(epsilon1))^2 - (tand(epsilon2))^2) + L2oC*cosd(epsilon2)*(tand(epsilon2))^2;
-
-%Apply linearized equation for coefficient of drag
-c_dw = 2 / sqrt(M^2 - 1) * (2*alpha^2 + gl_Squared + gu_Squared);
-
-%Alyx test
+%More accurate approximation, can use epsilon1 and epsilon2 instead of tan
+%since we assume small AoA
 gl = tand(epsilon1); 
 gu = tand(epsilon2);
 
 c_dw = (2/(sqrt(M^2 -1))) * (2*alpha^2 + gl^2 + gu^2);
-
 end
 
