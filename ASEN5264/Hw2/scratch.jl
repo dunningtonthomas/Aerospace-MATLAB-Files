ff03889c13327e1265678b95aae9b3f9c71e06ab
dunningtonthomas <dunningtonthomas@gmail.com>
2024-02-03 19:57:09 -0700

    Update

---
+++ ASEN5264/Hw2/scratch.jl
@@ -0,0 +1,11 @@
+using POMDPTools
+using POMDPModels
+using DMUStudent.HW2
+using POMDPs: states, actions
+using POMDPTools: ordered_states
+
+@show actions(grid_world) # prints the actions. In this case each action is a Symbol. Use ?Symbol to find out more.
+
+T = transition_matrices(grid_world)
+display(T) # this is a Dict that contains a transition matrix for each action
+
