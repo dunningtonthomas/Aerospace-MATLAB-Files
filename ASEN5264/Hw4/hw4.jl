e41073c6cda1c0a2d0976892a08c78028fc07f91
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-03-06 16:10:47 -0700

    Update hw4.jl

--- ASEN5264/Hw4/hw4.jl
+++ ASEN5264/Hw4/hw4.jl
@@ -1,13 +1,60 @@
 using DMUStudent.HW4
 using Plots
-#using POMDPs: actions, @gen, isterminal, discount, statetype, actiontype, simulate, states, initialstate, support
 using CommonRLInterface
 using Statistics: mean, std
 
 
 
-############ SARSA Implementation ###################
+############ Q Implementation ###################
+function Q_episode!(Q, env; epsilon=0.10, gamma=0.99, alpha=0.2)
+    start = time()
+    
+    function policy(s)
+        if rand() < epsilon
+            return rand(actions(env))
+        else
+            return argmax(a->Q[(s, a)], actions(env))
+        end
+    end
 
+    s = observe(env)
+    a = policy(s)
+    r = act!(env, a)
+    sp = observe(env)
+    hist = [s]
+
+    while !terminated(env)
+        a = policy(s)
+        r = act!(env, a)
+        sp = observe(env)
+
+        Q[(s,a)] += alpha*(r + gamma*maximum(Q[(sp, ap)] for ap in actions(env)) - Q[(s, a)])   # Update Q
+        
+        s = sp              # Update the state
+        push!(hist, sp)     # Add to the history
+    end
+
+    Q[(s,a)] += alpha*(r - Q[(s, a)])   # Update Q
+
+    return (hist=hist, Q = copy(Q), time=time()-start)
+end
+
+
+function Q!(env; n_episodes=100)
+    Q = Dict((s, a) => 0.0 for s in observations(env), a in actions(env))
+    episodes = []
+    
+    for i in 1:n_episodes
+        reset!(env)
+        push!(episodes, Q_episode!(Q, env; epsilon=max(0.1, 1-i/n_episodes), alpha=0.2))   # More explore at the beginning
+        #push!(episodes, sarsa_episode!(Q, env; epsilon=0.1))   # Non decaying epsilon
+    end
+    
+    return episodes
+end
+
+
+################## SARSA Learning ################################
 function sarsa_episode!(Q, env; epsilon=0.10, gamma=0.99, alpha=0.2)
     start = time()
     
@@ -49,14 +96,16 @@ function sarsa!(env; n_episodes=100)
     
     for i in 1:n_episodes
         reset!(env)
-        push!(episodes, sarsa_episode!(Q, env; epsilon=max(0.1, 1-i/n_episodes)))
+        push!(episodes, sarsa_episode!(Q, env; epsilon=max(0.1, 1-i/n_episodes), alpha=0.2))   # More explore at the beginning
+        #push!(episodes, sarsa_episode!(Q, env; epsilon=0.1))   # Non decaying epsilon
     end
     
     return episodes
 end
 
 
-function evaluate(env, policy; n_episodes=1000, max_steps=1000, γ=1.0)
+# Evaluate will use a given policy from the episodes and return the undiscounted cumulative reward
+function evaluate(env, policy; n_episodes=1000, max_steps=1000, gamma=1.0)
     returns = Float64[]
     for _ in 1:n_episodes
         t = 0
@@ -65,7 +114,7 @@ function evaluate(env, policy; n_episodes=1000, max_steps=1000, γ=1.0)
         s = observe(env)
         while !terminated(env)
             a = policy(s)
-            r += γ^t*act!(env, a)
+            r += gamma^t*act!(env, a)
             s = observe(env)
             t += 1
         end
@@ -75,18 +124,53 @@ function evaluate(env, policy; n_episodes=1000, max_steps=1000, γ=1.0)
 end
 
 
-
-
 ## Main
-
-# Simple grid world
+# Simple grid world environment
 m = HW4.gw
 env = convert(AbstractEnv, m)
-#HW4.render(m)
 
-sarsa_episodes = sarsa!(env, n_episodes=10000);
+# Run SARSA
+sarsa_episodes = sarsa!(env, n_episodes=100000);
+
+# Run Q
+Q_episodes = Q!(env, n_episodes=100000);
+
+# Plot Results Create Dictionary
+episodes = Dict("SARSA"=>sarsa_episodes, "Q"=>Q_episodes)
+
+# Create Steps in Environment Plot
+p = plot(xlabel="steps in environment", ylabel="avg return")
+n = 1000
+stop = 100000
+for (name, eps) in episodes
+    Q = Dict((s, a) => 0.0 for s in observations(env), a in actions(env))
+    xs = [0]
+    ys = [mean(evaluate(env, s->argmax(a->Q[(s, a)], actions(env))))]
+    for i in n:n:min(stop, length(eps))
+        newsteps = sum(length(ep.hist) for ep in eps[i-n+1:i])
+        push!(xs, last(xs) + newsteps)
+        Q = eps[i].Q
+        push!(ys, mean(evaluate(env, s->argmax(a->Q[(s, a)], actions(env)))))
+    end    
+    plot!(p, xs, ys, label=name)
+end
 
 
-finalEp = sarsa_episodes[length(sarsa_episodes)];
-HW4.render(m, color=s->maximum(map(a->finalEp.Q[(s,a)], actions(env))))
+# Create Wall Clock Time Plot
+p2 = plot(xlabel="wall clock time", ylabel="avg return")
+for (name,eps) in episodes
+    Q = Dict((s, a) => 0.0 for s in observations(env), a in actions(env))
+    xs = [0.0]
+    ys = [mean(evaluate(env, s->argmax(a->Q[(s, a)], actions(env))))]
+    for i in n:n:min(stop, length(eps))
+        newtime = sum(ep.time for ep in eps[i-n+1:i])
+        push!(xs, last(xs) + newtime)
+        Q = eps[i].Q
+        push!(ys, mean(evaluate(env, s->argmax(a->Q[(s, a)], actions(env)))))
+    end    
+    plot!(p2, xs, ys, label=name)
+end
+
+
+
 
