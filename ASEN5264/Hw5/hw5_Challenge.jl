6451274c96c53861539640f1d2117a0dbd55e7ed
dunningtonthomas <dunningtonthomas@gmail.com>
2024-03-21 00:19:40 -0600

    Update hw5_Challenge.jl

--- ASEN5264/Hw5/hw5_Challenge.jl
+++ ASEN5264/Hw5/hw5_Challenge.jl
@@ -7,6 +7,7 @@ using Flux
 using CommonRLInterface.Wrappers: QuickWrapper
 using Plots
 using DMUStudent.HW5: HW5, mc
+using Statistics: mean
 # using VegaLite
 # using ElectronDisplay # not needed if you're using a notebook or something that can display graphs
 # using DataFrames: DataFrame
@@ -29,6 +30,18 @@ function dqn(env)
     # Best Q function based on eval
     Q_best = deepcopy(Q)
 
+    # Average reward vector and loss vector
+    epochReward = []
+    lossVec = []
+
+    # HYPERPARAMETERS
+    # Size of the buffer
+    bufferSize = 50000
+    e_orig = 0.1
+    epsilon = 0.1
+    n = 10000
+    epochs = 1000
+
     # Epsilon Greedy Policy
     function policy(s, epsilon=0.1)
         if rand() < epsilon
@@ -40,10 +53,12 @@ function dqn(env)
     end
 
     # Gain experience function, appends the buffer
-    function experience(buffer, n)
+    function experience(buffer, n, epsilon)
         # See if terminal
         done = terminated(env)
         i = 1
+        # Recently added buffer
+        recentBuffer = []
         # Loop through n steps in the environment and add to the buffer
         while i <= n && !done
             s = observe(env)
@@ -53,13 +68,14 @@ function dqn(env)
             done = terminated(env)
             experience_tuple = (s, a_ind, r, sp, done)
             push!(buffer, experience_tuple)                 # Add to the experience
+            push!(recentBuffer, experience_tuple)                 # Add to the experience
             i = i + 1
         end
-        return buffer
+        return buffer, recentBuffer
     end
 
     # Resets the buffer and gains experience
-    function reset_buffer(n)
+    function reset_buffer(n, epsilon)
         # Reset buffer
         buffer = []
         i = 1
@@ -100,23 +116,32 @@ function dqn(env)
 
     
     # Instantiate the buffer, 1000 steps for each episode
-    n = 10000
     buffer = []
-    buffer = experience(buffer, n)      # Initial buffer episode
+    buffer, recentBuffer = experience(buffer, n, e_orig)      # Initial buffer episode
     bestReward = -1000
-
-    # Number of epochs
-    epochs = 1000
 
     for epoch in 1:epochs
         # Rest the environment
         reset!(env)
 
+        # Set Optimizer
+        opt = Flux.setup(ADAM(0.0005), Q)
+
+        # Decay epsilon, goes to 0 in half of the epochs
+        # epsilon = e_orig * (1 - epoch/(epochs/2))
+        epsilon = epsilon * 0.99
+
+
         # Gain experience
-        buffer = experience(buffer, n)
+        buffer, recentBuffer = experience(buffer, n, epsilon)
 
+        # Reset the target network
+        # if epoch % 10 == 0
+        #     # Set the target Q network
+        #     Q_target = deepcopy(Q)
+        # end
 
-        # Q Training Loss Function
+        Q_target = deepcopy(Q)
         function loss(Q, s, a_ind, r, sp, done)
             # Discount factor
             g = 0.99
@@ -129,41 +154,52 @@ function dqn(env)
         end
 
         # Get random data from the buffer
-        data = rand(buffer, 1000)
+        data = rand(buffer, 2000)
 
         # Train based on random data in the buffer
-        Flux.Optimise.train!(loss, Q, data, Flux.setup(ADAM(0.0005), Q))
+        Flux.Optimise.train!(loss, Q, data, opt)
 
-        # Trash old buffer data
-        if epoch % 20 == 0
-            buffer = reset_buffer(n)
-            
-            # Set the target Q network
-            Q_target = deepcopy(Q)
-        end
-
-        # Evaluate Epoch
-        num_eps = 50
-        avgReward = eval(Q, num_eps)
-        if avgReward > bestReward           # Update if new best is found
+        # Evaluate Epoch, Update if new best is found
+        num_eps = 500
+        #avgReward = eval(Q, num_eps)
+        score = HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], n_episodes=num_eps)
+        avgReward = score[1]
+        push!(epochReward, avgReward)
+        if avgReward > bestReward           
             bestReward = avgReward
             Q_best = deepcopy(Q)
+            buffer = recentBuffer           # Reset the buffer with the good buffer data
         end
+
+        # Shift the buffer if exceeding buffer size
+        if length(buffer) > bufferSize
+            buffer = buffer[end-bufferSize:end]
+        end
+
+        # Calculate the loss
+        push!(lossVec, mean([loss(Q, s, a_ind, r, sp, done) for (s, a_ind, r, sp, done) in data]))
 
         # Output data
         print("Epoch: ", epoch, "\t Buffer Size: ", length(buffer), "\t Average Reward: ", avgReward, "\n")
     end
 
     # Make sure to evaluate, print, and plot often! You will want to save your best policy.
-    return Q_best
+    return Q_best, epochReward, lossVec
 end
 
 # Call DQN to get best Q function
-Q = dqn(env)
+Q, epochReward, lossVec = dqn(env)
+
 
 # Evaluate
 HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], n_episodes=100) # you will need to remove the n_episodes=100 keyword argument to create a json file; evaluate needs to run 10_000 episodes to produce a json
 #HW5.evaluate(s->actions(env)[argmax(Q(s[1:2]))], "thomas.dunnington@colorado.edu")
+
+# Plot the learning curve for the average reward
+rewardPlot = plot(1:length(epochReward), epochReward, label="Average Reward", xlabel="Epoch", ylabel="Average Reward", title="Learning Curve Rewards")
+hline!(rewardPlot, [40], line=:dash, label="Reward of 40")
+
+lossPlot = plot(1:length(lossVec), lossVec, label="Loss", xlabel="Epoch", ylabel="DQN Loss", title="Learning Curve Loss")
 
 #----------
 # Rendering
