691be767b72f6e4904bf31c6c089a43f0411879a
dunningtonthomas <dunningtonthomas@gmail.com>
2024-04-07 23:25:45 -0600

    Create DMU Hw6

---
+++ ASEN5264/Hw6/hw6_Q1.jl
@@ -0,0 +1,94 @@
+using POMDPs
+using DMUStudent.HW6
+using POMDPTools: transition_matrices, reward_vectors, SparseCat, Deterministic, RolloutSimulator, DiscreteBelief, FunctionPolicy, ordered_states, ordered_actions, DiscreteUpdater
+using QuickPOMDPs: QuickPOMDP
+using POMDPModels: TigerPOMDP, TIGER_LEFT, TIGER_RIGHT, TIGER_LISTEN, TIGER_OPEN_LEFT, TIGER_OPEN_RIGHT
+using NativeSARSOP: SARSOPSolver
+using POMDPTesting: has_consistent_distributions
+
+##################
+# Problem 1: Tiger
+##################
+
+#--------
+# Updater
+#--------
+
+struct HW6Updater{M<:POMDP} <: Updater
+    m::M
+end
+
+function POMDPs.update(up::HW6Updater, b::DiscreteBelief, a, o)
+    bp_vec = zeros(length(states(up.m)))
+    bp_vec[1] = 1.0
+
+    # Fill in code for belief update
+    # Note that the ordering of the entries in bp_vec must be consistent with stateindex(m, s) (the container returned by states(m) does not necessarily obey this order)
+
+    return DiscreteBelief(up.m, bp_vec)
+end
+
+# Note: you can access the transition and observation probabilities through the POMDPs.transtion and POMDPs.observation, and query individual probabilities with the pdf function. For example if you want to use more mathematical-looking functions, you could use the following:
+# Z(o | a, s') can be programmed with
+Z(m::POMDP, a, sp, o) = pdf(observation(m, a, sp), o)
+# T(s' | s, a) can be programmed with
+T(m::POMDP, s, a, sp) = pdf(transition(m, s, a), sp)
+# POMDPs.transtion and POMDPs.observation return distribution objects. See the POMDPs.jl documentation for more details.
+
+# This is needed to automatically turn any distribution into a discrete belief.
+function POMDPs.initialize_belief(up::HW6Updater, distribution::Any)
+    b_vec = zeros(length(states(up.m)))
+    for s in states(up.m)
+        b_vec[stateindex(up.m, s)] = pdf(distribution, s)
+    end
+    return DiscreteBelief(up.m, b_vec)
+end
+
+# Note: to check your belief updater code, you can use POMDPTools: DiscreteUpdater. It should function exactly like your updater.
+
+#-------
+# Policy
+#-------
+
+struct HW6AlphaVectorPolicy{A} <: Policy
+    alphas::Vector{Vector{Float64}}
+    alpha_actions::Vector{A}
+end
+
+function POMDPs.action(p::HW6AlphaVectorPolicy, b::DiscreteBelief)
+
+    # Fill in code to choose action based on alpha vectors
+
+    return first(actions(b.pomdp))
+end
+
+beliefvec(b::DiscreteBelief) = b.b # this function may be helpful to get the belief as a vector in stateindex order
+
+#------
+# QMDP
+#------
+
+function qmdp_solve(m, discount=discount(m))
+
+    # Fill in Value Iteration to compute the Q-values
+
+    acts = actiontype(m)[]
+    alphas = Vector{Float64}[]
+    for a in actions(m)
+
+        # Fill in alpha vector calculation
+        # Note that the ordering of the entries in the alpha vectors must be consistent with stateindex(m, s) (states(m) does not necessarily obey this order, but ordered_states(m) does.)
+        
+    end
+    return HW6AlphaVectorPolicy(alphas, acts)
+end
+
+m = TigerPOMDP()
+
+qmdp_p = qmdp_solve(m)
+# Note: you can use the QMDP.jl package to verify that your QMDP alpha vectors are correct.
+sarsop_p = solve(SARSOPSolver(), m)
+up = HW6Updater(m)
+
+@show mean(simulate(RolloutSimulator(max_steps=500), m, qmdp_p, up) for _ in 1:5000)
+@show mean(simulate(RolloutSimulator(max_steps=500), m, sarsop_p, up) for _ in 1:5000)
