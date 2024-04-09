d8d50ba97ccedcd112abbc5ff1f03955e50ba3bd
dunningtonthomas <dunningtonthomas@gmail.com>
2024-04-09 01:23:54 -0600

    Update hw6_Q1.jl

--- ASEN5264/Hw6/hw6_Q1.jl
+++ ASEN5264/Hw6/hw6_Q1.jl
@@ -19,14 +19,22 @@ struct HW6Updater{M<:POMDP} <: Updater
 end
 
 function POMDPs.update(up::HW6Updater, b::DiscreteBelief, a, o)
-    bp_vec = zeros(length(states(up.m)))
+    stateVec = collect(states(up.m))
+    bp_vec = zeros(length(stateVec))
     bp_vec[1] = 1.0
+    # Probability functions
+    Z(m::POMDP, a, sp, o) = pdf(observation(m, a, sp), o)
+    T(m::POMDP, s, a, sp) = pdf(transition(m, s, a), sp)
 
-    # Fill in code for belief update
     # Note that the ordering of the entries in bp_vec must be consistent with stateindex(m, s) (the container returned by states(m) does not necessarily obey this order)
-
+    for i in 1:length(bp_vec)
+        sp = stateVec[i]
+        bp_vec[i] = Z(up.m, a, sp, o)*sum(s -> T(up.m, s, a, sp)*pdf(b, s), stateVec)
+    end
+    bp_vec = bp_vec ./ sum(bp_vec)
     return DiscreteBelief(up.m, bp_vec)
 end
+
 
 # Note: you can access the transition and observation probabilities through the POMDPs.transtion and POMDPs.observation, and query individual probabilities with the pdf function. For example if you want to use more mathematical-looking functions, you could use the following:
 # Z(o | a, s') can be programmed with
@@ -89,6 +97,8 @@ qmdp_p = qmdp_solve(m)
 # Note: you can use the QMDP.jl package to verify that your QMDP alpha vectors are correct.
 sarsop_p = solve(SARSOPSolver(), m)
 up = HW6Updater(m)
+up2 = DiscreteUpdater(m)
 
-@show mean(simulate(RolloutSimulator(max_steps=500), m, qmdp_p, up) for _ in 1:5000)
+#@show mean(simulate(RolloutSimulator(max_steps=500), m, qmdp_p, up) for _ in 1:5000)
 @show mean(simulate(RolloutSimulator(max_steps=500), m, sarsop_p, up) for _ in 1:5000)
+#@show mean(simulate(RolloutSimulator(max_steps=500), m, sarsop_p, up2) for _ in 1:5000)
