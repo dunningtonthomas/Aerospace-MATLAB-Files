2c23e760d963d4f9c5fe622ed70873484d8a66c9
dunningtonthomas <dunningtonthomas@gmail.com>
2024-04-10 00:26:44 -0600

    Update hw6_Q1.jl

--- ASEN5264/Hw6/hw6_Q1.jl
+++ ASEN5264/Hw6/hw6_Q1.jl
@@ -5,6 +5,8 @@ using QuickPOMDPs: QuickPOMDP
 using POMDPModels: TigerPOMDP, TIGER_LEFT, TIGER_RIGHT, TIGER_LISTEN, TIGER_OPEN_LEFT, TIGER_OPEN_RIGHT
 using NativeSARSOP: SARSOPSolver
 using POMDPTesting: has_consistent_distributions
+using LinearAlgebra
+
 
 ##################
 # Problem 1: Tiger
@@ -14,12 +16,20 @@ using POMDPTesting: has_consistent_distributions
 # Updater
 #--------
 
+# Note: you can access the transition and observation probabilities through the POMDPs.transtion and POMDPs.observation, and query individual probabilities with the pdf function. For example if you want to use more mathematical-looking functions, you could use the following:
+# Z(o | a, s') can be programmed with
+Z(m::POMDP, a, sp, o) = pdf(observation(m, a, sp), o)
+# T(s' | s, a) can be programmed with
+T(m::POMDP, s, a, sp) = pdf(transition(m, s, a), sp)
+# POMDPs.transtion and POMDPs.observation return distribution objects. See the POMDPs.jl documentation for more details.
+
+
 struct HW6Updater{M<:POMDP} <: Updater
     m::M
 end
 
 function POMDPs.update(up::HW6Updater, b::DiscreteBelief, a, o)
-    stateVec = collect(states(up.m))
+    stateVec = collect(ordered_states(up.m))
     bp_vec = zeros(length(stateVec))
     bp_vec[1] = 1.0
     # Probability functions
@@ -27,21 +37,13 @@ function POMDPs.update(up::HW6Updater, b::DiscreteBelief, a, o)
     T(m::POMDP, s, a, sp) = pdf(transition(m, s, a), sp)
 
     # Note that the ordering of the entries in bp_vec must be consistent with stateindex(m, s) (the container returned by states(m) does not necessarily obey this order)
-    for i in 1:length(bp_vec)
-        sp = stateVec[i]
+    for (i, sp) in enumerate(stateVec)
         bp_vec[i] = Z(up.m, a, sp, o)*sum(s -> T(up.m, s, a, sp)*pdf(b, s), stateVec)
     end
     bp_vec = bp_vec ./ sum(bp_vec)
     return DiscreteBelief(up.m, bp_vec)
 end
 
-
-# Note: you can access the transition and observation probabilities through the POMDPs.transtion and POMDPs.observation, and query individual probabilities with the pdf function. For example if you want to use more mathematical-looking functions, you could use the following:
-# Z(o | a, s') can be programmed with
-Z(m::POMDP, a, sp, o) = pdf(observation(m, a, sp), o)
-# T(s' | s, a) can be programmed with
-T(m::POMDP, s, a, sp) = pdf(transition(m, s, a), sp)
-# POMDPs.transtion and POMDPs.observation return distribution objects. See the POMDPs.jl documentation for more details.
 
 # This is needed to automatically turn any distribution into a discrete belief.
 function POMDPs.initialize_belief(up::HW6Updater, distribution::Any)
@@ -64,21 +66,28 @@ struct HW6AlphaVectorPolicy{A} <: Policy
 end
 
 function POMDPs.action(p::HW6AlphaVectorPolicy, b::DiscreteBelief)
+    # Initialize the belief distribution and the alpha vectors
     beliefvec(b::DiscreteBelief) = b.b
-    alphaVecs = p.alphas
-    alphaActions = p.alpha_actions
+    # More compact:
+    i = argmax([dot(alpha, beliefvec(b)) for alpha in p.alphas])
+    return p.alpha_actions[i]
 
-    # Fill in code to choose action based on alpha vectors
-    # Find the most likely state based on the belief vector
-    stateInd = argmax(beliefvec(b))
+    # alphaVecs = p.alphas
+    # alphaActions = p.alpha_actions
+    # best_action = 0
+    # best_value = -Inf
 
-    # Get the alpha vector corresponding to the current state
-    stateAlpha = alphaVecs[stateInd]
-    
-    # Return the action associated with the maximum reward for the given alpha vector
-    return argmax(a->stateAlpha, alphaActions)
+    # # Maximize the dot product of the alpha vector and the belief vector
+    # for (i, alphaVec) in enumerate(alphaVecs)
+    #     action_value = dot(alphaVec, beliefvec(b))
 
-    #return first(actions(b.pomdp))
+    #     # Update best action and values
+    #     if action_value > best_value
+    #         best_value = action_value
+    #         best_action = i
+    #     end
+    # end   
+    # return alphaActions[best_action]
 end
 
 beliefvec(b::DiscreteBelief) = b.b # this function may be helpful to get the belief as a vector in stateindex order
@@ -88,19 +97,35 @@ beliefvec(b::DiscreteBelief) = b.b # this function may be helpful to get the bel
 #------
 
 function qmdp_solve(m, discount=discount(m))
+    # Value Iteration to compute the Q-values
+    Ts = transition_matrices(m, sparse=true)
+    Rs = reward_vectors(m)
+    n = length(first(values(Rs)))
+    V = zeros(n)
+    oldV = ones(n)
+    while maximum(abs, V-oldV) > 1e-3   # Tolerance
+        oldV[:] = V
+        V[:] = max.((Rs[a] + discount*Ts[a]*V for a in keys(Rs))...)
+    end
 
-    # Fill in Value Iteration to compute the Q-values
 
+    # Compute the alpha vectors using the calculated value function
     acts = actiontype(m)[]
     alphas = Vector{Float64}[]
-    for a in actions(m)
+    for a in ordered_actions(m)
+        push!(acts, a)  # Add the action
+        alpha = zeros(length(states(m)))
 
         # Fill in alpha vector calculation
         # Note that the ordering of the entries in the alpha vectors must be consistent with stateindex(m, s) (states(m) does not necessarily obey this order, but ordered_states(m) does.)
-        
+        for (i, s) in enumerate(ordered_states(m))
+            alpha[stateindex(m, s)] = reward(m, s, a) + discount * sum(T(m, s, a, sp) * V[stateindex(m, s)] for sp in ordered_states(m))             
+        end
+        push!(alphas, alpha)
     end
     return HW6AlphaVectorPolicy(alphas, acts)
 end
+
 
 # POMDP definition
 m = TigerPOMDP()
@@ -115,6 +140,6 @@ up2 = DiscreteUpdater(m)
 
 # Test the policy and action function --> Call the action function based on an initial belief and state
 
-#@show mean(simulate(RolloutSimulator(max_steps=500), m, qmdp_p, up) for _ in 1:5000)
+@show mean(simulate(RolloutSimulator(max_steps=500), m, qmdp_p, up) for _ in 1:5000)
 @show mean(simulate(RolloutSimulator(max_steps=500), m, sarsop_p, up) for _ in 1:5000)
 #@show mean(simulate(RolloutSimulator(max_steps=500), m, sarsop_p, up2) for _ in 1:5000)
