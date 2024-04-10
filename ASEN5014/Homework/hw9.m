fcfd493d5f567b0077269f73e1faebb3c0976491
dunningtonthomas <dunningtonthomas@gmail.com>
2024-04-09 22:24:28 -0600

    Update hw9.m

--- ASEN5014/Homework/hw9.m
+++ ASEN5014/Homework/hw9.m
@@ -29,6 +29,35 @@ C1 = controllMat(A,B1);
 C2 = controllMat(A,B2);
 
 
+%% Problem 4
+A = [-2, -3.5, -2; 1, 0, 0; 0, 1, 0];
+B = [4;0;0];
+C = [0, 1, 1];
+D = 0;
+
+poles = [-0.5, -1 + 3*1j, -1 - 3*1j];
+K = place(A,B,poles);
+
+[testV, testE] = eig(A - B*K);
+
+
+
+
+%% Problem 5
+A = [12, -14, 10, -26, 8; 0, 10, 4, -8, -4; 0, -8, 22, -2, 8; 0, 0, 0, 24, 0; 0, -16, 8, -22, 22];
+B = [4;2;2;0;1];
+C = [3,3,-3,6,0; 6,4,4,-8,-10];
+D = [0;0];
+
+Cmat = controllMat(A,B);
+[eigVec, eigVal] = eig(A);
+T = eigVec;
+Lambda = inv(T)*A*T;
+Btild = inv(T)*B;
+
+Ctild = controllMat(Lambda, Btild);
+Cprime = C*T;
+
 
 %% Functions
 function out = controllMat(A,B)
