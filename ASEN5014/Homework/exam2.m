d0f4e00ab7f2162bca57d449681130774d694a98
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2024-04-22 13:44:49 -0600

    Update exam2.m

--- ASEN5014/Homework/exam2.m
+++ ASEN5014/Homework/exam2.m
@@ -14,6 +14,7 @@ A = [1,1,1; 0,1,1;1,0,0];
 
 syms a b c
 B = [a;b;c];
+B = [1;0;0];
 
 C = [B A*B A^2*B];
 
@@ -34,6 +35,39 @@ B = [1;1;1;2;2;0];
 C = [1,2,0,0,0,0];
 D = 0;
 
-[vec, vals] = eig(A);
+[T, vals] = eig(A);
+
+diag = inv(T)*A*T;
+Btild = inv(T)*B;
+Ctild = C*T;
+
+
+
+%% Problem 4
+A = [0,2,1; -1,0,1;3,-2,1;0,4,1;1,2,1];
+
+LN = null(A');
+y = LN(:,1);
+
+W = [A y];
+
+
+A = [1,1,1,1,; 2,4,6,3; 0,2,4,1; 1,-1,-3,0];
+y = [5;25;15;-10];
+
+Aug = [A y];
+Ared = A(1:2, :);
+yred = y(1:2);
+
+xln = Ared'*inv(Ared*Ared')*yred;
+
+RN = null(A);
+
+x1 = xln + RN(:,1);
+x2 = xln + RN(:,2);
+
+
+
+
 
 
