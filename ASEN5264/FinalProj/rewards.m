ce4927bb172d0190bcc82cff24811003678a964d
dunningtonthomas <dunningtonthomas@gmail.com>
2024-08-26 19:18:29 -0600

    Create rewards.m

---
+++ ASEN5264/FinalProj/rewards.m
@@ -0,0 +1,10 @@
+% Plot the reward function for the Rocket Booster MDP
+close all; clear; clc;
+
+
+%% Define functions
+xTarg = 0;
+x = linspace(-100,100,1000);
+theta = linspace(-pi, pi, 100);
+
+
