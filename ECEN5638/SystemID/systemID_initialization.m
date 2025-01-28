b72359d393025f70a7afa6b5fa3aae47bcdb2488
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2025-01-28 11:56:02 -0700

    ECEN Update

--- ECEN5638/SystemID/systemID_initialization.m
+++ ECEN5638/SystemID/systemID_initialization.m
@@ -2,11 +2,11 @@
 close all; clear; clc;
 
 %% Input Signal
-T = 1;
-Ts = 0.01;
-Ttrans = 5;
-K = 1;
-N = 20; % Number of frequencies we want to excite in the system
+T = 15;
+Ts = 0.002;
+Ttrans = 4.9;
+K = 5;
+N = 100; % Number of frequencies we want to excite in the system
 omega0 = 50;
 
 
